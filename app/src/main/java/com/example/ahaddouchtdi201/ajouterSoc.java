12459776a907ecf52a91391c7ae16c83b635951c
mir <ahaddouchadam2@gmail.com>
2022-06-03 15:59:31 +0100

    Q7

---
+++ app/src/main/java/com/example/ahaddouchtdi201/ajouterSoc.java
@@ -0,0 +1,14 @@
+package com.example.ahaddouchtdi201;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ajouterSoc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_soc);
+    }
+}
