a69e2eb99b7b2d07d3a2b286f9cf1b3c97981787
mir <ahaddouchadam2@gmail.com>
2022-06-03 16:32:12 +0100

    Q7

--- app/src/main/java/com/example/ahaddouchtdi201/MainActivity.java
+++ app/src/main/java/com/example/ahaddouchtdi201/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.ahaddouchtdi201;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1,b2,b3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.btnadd);
+        b2=findViewById(R.id.btnedit);
+        b2=findViewById(R.id.btnlist);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent aj=new Intent(MainActivity.this,ajouterSoc.class);
+                startActivity(aj);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,editeSoc.class);
+                startActivity(intent);
+            }
+        });
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,listSoc.class);
+                startActivity(i);
+            }
+        });
+
+
+
     }
 }
